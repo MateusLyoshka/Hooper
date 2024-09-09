8d70ea6a066cd339e4def072bbbc824f9da64159
Mateus santos fernandes <mateusf.2022@alunos.utfpr.edu.br>
2024-09-09 14:01:15 -0300

    home body

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,82 @@
 import Header from "@/components/myComponents/header/header"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+
+const userinfo = {
+    name: "gado",
+};
+
+const games = [
+    {
+        game:"1", teamA:"A", teamB:"B", mounth:"August", day:"12", year:"2024", time:"18:00", 
+    },
+    {game:"2", teamA:"C", teamB:"D", mounth:"August", day:"12", year:"2024", time:"21:00"}
+]
+
+const recentGame = {teamA:"A", teamB:"B", pointsA:"80", pointsB:"75", score:"22", assists:"5", rebounds:"8"}
+
 
 const Home = () => {
-    return(
-        <div className="w-full h-screen">
-            <div>
-                <Header/>
-            </div>
-            <div></div>
-            <div></div>
+
+    return (
+      <div className="w-full h-screen">
+        <div>
+          <Header />
         </div>
-    )
+        <div>
+          <div className="mt-10 p-2">
+            <Card className="p-4">
+              <CardHeader className="p-3 text-3xl">
+                Welcome {userinfo.name}
+              </CardHeader>
+              <div className="w-full bg-gray-500 h-[1px] opacity-50 mb-4"></div>
+              <CardContent>
+                <div className="">
+                  Get ready to hit the court and track your progress with Hoops.
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="mt-10 p-2">
+            <Card className="p-4">
+              <CardHeader className="p-3 text-3xl">Upcoming matches</CardHeader>
+              <div className="w-full bg-gray-500 h-[1px] opacity-50 mb-4"></div>
+              <CardContent>
+                {games.map((itens, index) => (
+                  <div key={index} className="mt-2">
+                    <div>
+                      Game {itens.game}: Team {itens.teamA} vs Team{" "}
+                      {itens.teamB} - {itens.mounth} {itens.day}, {itens.year},{" "}
+                      {itens.time}
+                    </div>
+                  </div>
+                ))}
+              </CardContent>
+            </Card>
+          </div>
+          <div className="mt-10 p-2">
+            <Card className="p-4">
+              <CardHeader className="p-3 text-3xl">Upcoming matches</CardHeader>
+              <div className="w-full bg-gray-500 h-[1px] opacity-50 mb-4"></div>
+              <CardContent>
+                <div className="mt-2">
+                  <div className="space-y-1">
+                    <div>
+                      Last Game: Team{recentGame.teamA} {recentGame.pointsA} vs
+                      Team {recentGame.pointsB} {recentGame.teamB}
+                    </div>
+                    <div>Points Scored: {recentGame.score}</div>
+                    <div>Assists: {recentGame.assists}</div>
+                    <div>Rebounds: {recentGame.rebounds}</div>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    );
 }
 
 export default Home
--- src/components/myComponents/header/header.tsx
+++ src/components/myComponents/header/header.tsx
@@ -1,19 +1,25 @@
-import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
 
 const Header = () => {
-    const headerButtons = ["Profile", "Games", "Logout"]
+    const headerButtons = ["Profile","Next Matches", "Create Match", "Logout"]
 
     return(
-        <div className="w-full h-36 text-white bg-gray-700 flex flex-col items-center justify-center">
-            <div className="text-5xl font-semibold">Hoops</div>
+        <div className="w-full h-40 text-white bg-gray-700 flex flex-col items-center justify-center">
+            <div className="text-5xl font-semibold p-6">Hoops</div>
             <div className="flex">
-                {headerButtons.map((item, index) => (
-                    <div key={index} className="flex">
-                        <Button className=" hover:bg-gray-800 hover:text-white border-none rounded-none w-20 mt-4" variant={"ghost"}>{item}</Button>
-                    </div>
-                ))}
-            </div>
+            {headerButtons.map((item, index) => (
+            <Link
+              key={index}
+              href={`/home/${item.toLowerCase()}`}
+              className={cn(buttonVariants({ variant: "ghost" }), "p-8 hover:bg-opacity-5 hover:bg-white hover:text-white rounded-none")}
+            >
+              {item}
+            </Link>
+          ))}
         </div>
+    </div>
     )
 }
 
